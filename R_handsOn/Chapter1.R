e78cbe8a085d0f3afd2cf95c6ad7ce79b492fb69
Uladzimir Sapeshka <usapeshka@gmail.com>
2018-12-18 20:47:57 -0600

    Notes on R notation

--- R_handsOn/Chapter1.R
+++ R_handsOn/Chapter1.R
@@ -26,7 +26,7 @@ roll <- function(){
 # Examples of using quicplot qplot
   # Scatter Plot
   x <- c(-1, -0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1)
-  y <- exp(x)
+  y <- x^2
   qplot(x,y)
   
   # Histogramms (qplot makes histograms when only single vector is provided as an argument)
@@ -45,7 +45,114 @@ roll <- function(){
 # repeat an R command, and then give it the command you wish to repeat.  
   args(replicate)
   
-  res <- replicate(1000000,roll3())
+  res <- replicate(100000,roll3())
+  
   qplot(res,binwidth = 1)
   
+# Chapter 2
   
+# Attributes names, dim, classes
+  # To look up the value of attributes
+  names(res)
+  dim(res)
+  class(res)  
+  typeof(res)
+  attributes(res)
+  
+names(die) = c("one","two","three","four","five","six")
+names(die)  
+  attributes(die)
+die  
+  # To remove object attribute set it to NULL  
+names(die) <-NULL
+  attributes(die)  
+die  
+  # Set dimentions attributes feed the vector with dimentions
+dim(die)<-c(2,3)
+attributes(die)
+dim(die)
+  
+  # Same is atchieved by using matrix command with a little more control
+dim(die) <- NULL
+die
+
+matrix(die,nrow = 2) # fills the matrix by column 
+die
+matrix(die,nrow = 2, byrow = TRUE)
+new <- matrix(die,nrow = 2, byrow = TRUE) 
+names(new)<- c("one","two","three","four","five")
+names(new) <- NULL
+names(new)
+
+mx <- matrix(c("ace","king","queen","jack","ten","spades","spades","spades","spades","spades"),ncol = 2)
+mx
+
+# Changing the dimention of an ojectect doesn't change it's type but does change its class
+typeof(die)
+class(die)
+dim(die) <- c(2,3)
+typeof(die)
+class(die)
+dim(die) <- NULL
+die<-c(1,2,3,4,5,6)
+typeof(die)
+
+# Factor is a class used to track categorical data
+
+gender <- factor(c("m","f","m","m"))
+unclass(gender)
+typeof(gender)
+attributes(gender)
+
+typeof(as.character(gender))
+
+# Special card
+typeof(c(1,1L,3L))
+
+sp.card <- factor(c("ace","hearts",1))
+sp.card
+
+# Can convert into specific data form when there is a logical way to do this
+
+as.character("1")
+as.numeric(TRUE)
+as.logical(FALSE)
+
+# Lists can store any type of data 
+card <- list ("ace","hearts",1)
+typeof(card)
+card[[1]]
+card[[2]]
+
+
+
+# Data frames are two dimentional version of  lists
+
+df <- data.frame( face = c("ace","two","six"), suit = c("clubs","diamond","diamond"), value = c(1,2,3))
+df
+
+typeof(df)
+class(df)
+attributes(df)
+str(df) # Created factors 
+
+df <- data.frame(face = c("ace", "two", "six"),
+                 suit = c("clubs", "clubs", "clubs"), value = c(1, 2, 3),
+                 stringsAsFactors = FALSE)
+# StringsAsFactors = False prevents R to declear ton's of factors
+df
+
+
+
+write.csv(tmp, file = "cards.csv", row.names = FALSE)
+getwd()
+
+# Chapter 4 R Notation
+
+# Positive integers
+# Negative integers
+# Zero
+# Blank spaces
+# Logical values
+# Names
+
---
+++ R_handsOn/Chapter2.R
@@ -0,0 +1,14 @@
+#Project 2 Playing Cards
+int <- c(1L,2L) # L after number indicates to R that this is an integer
+text <- c("ace","black")
+# typeof askes what type of object we are dealing with
+die<-1:6
+# integer
+typeof(die)
+die <-c(1,2,3,4,5,6)
+typeof(die)
+# double
+
+# Precision examples
+sqrt(2)^2 - 2
+#suppose to be zero
